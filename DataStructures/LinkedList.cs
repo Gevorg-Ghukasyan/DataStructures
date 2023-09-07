b3dfdf44663abf550dc0e68f5661a9000f1b6bb0
gevorggukasyan24@mail.ru <gevorggukasyan24@mail.ru>
2023-09-07 16:44:41 +0400

    Добавьте файлы проекта.

---
+++ DataStructures/LinkedList.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    class Node<T>
+    {
+        private T _value;
+        private Node<T> _next;
+        public T Value
+        {
+            get { return _value; }
+            set { _value = value; }
+        }
+        public Node<T> Next
+        {
+            get { return _next; }
+            set { _next = value; }
+        }
+
+    }
+    class LinkedListImpl<T>
+    {
+        private Node<T> _head;
+
+        public void Add(T value)
+        {
+            Node<T> newNode = new Node<T>();
+            newNode.Value = value;
+
+            if (_head == null)
+            {
+                _head = newNode;
+            }
+            else
+            {
+                Node<T> currentNode = _head;
+                while (currentNode.Next != null)
+                {
+                    currentNode = currentNode.Next;
+                }
+                currentNode.Next = newNode;
+            }
+        }
+        public void AddAll(IEnumerable<T> collection)
+        {
+            foreach (T item in collection)
+            {
+                Add(item);
+            }
+        }
+        public void Print()
+        {
+            Node<T> currentNode = _head;
+            while (currentNode != null)
+            {
+                Console.Write(currentNode.Value + " ");
+                currentNode = currentNode.Next;
+            }
+            Console.WriteLine();
+        }
+        public void Delete(T value)
+        {
+            if (_head == null)
+            {
+                return;
+            }
+
+            if (_head != null && _head.Value.Equals(value))
+            {
+                _head = _head.Next;
+                return;
+            }
+
+            Node<T> currentNode = _head;
+            while (currentNode.Next != null)
+            {
+                if (currentNode.Next.Value.Equals(value))
+                {
+                    currentNode.Next = currentNode.Next.Next;
+                    return;
+                }
+                currentNode = currentNode.Next;
+            }
+        }
+        public void DeleteAtIndex(int index)
+        {
+
+            if (index < 0 || index >= GetLength())
+            {
+                throw new ArgumentOutOfRangeException("Index is out of range.");
+            }
+
+            if (index == 0)
+            {
+                _head = _head.Next;
+                return;
+            }
+
+            Node<T> currentNode = _head;
+            Node<T> previousNode = null;
+            int currentIndex = 1;
+
+            while (currentNode != null && currentIndex != index)
+            {
+                previousNode = currentNode;
+                currentNode = currentNode.Next;
+                currentIndex++;
+            }
+
+            if (currentNode != null)
+            {
+                previousNode.Next = currentNode.Next;
+            }
+        }
+        public void ChangeValue(int index, T newValue)
+        {
+            Node<T> currentNode = _head;
+            int currentIndex = 0;
+            while (currentNode != null)
+            {
+                if (currentIndex == index)
+                {
+                    currentNode.Value = newValue;
+                    return;
+                }
+                currentNode = currentNode.Next;
+                currentIndex++;
+            }
+        }
+        private int GetLength()
+        {
+            int length = 0;
+            Node<T> currentNode = _head;
+            while (currentNode != null)
+            {
+                length++;
+                currentNode = currentNode.Next;
+            }
+            return length;
+        }
+        public void Reverse()
+        {
+            Node<T> previousNode = null;
+            Node<T> currentNode = _head;
+            Node<T> nextNode = null;
+
+            while (currentNode != null)
+            {
+                nextNode = currentNode.Next;
+                currentNode.Next = previousNode;
+
+                previousNode = currentNode;
+                currentNode = nextNode;
+            }
+
+            _head = previousNode;
+        }
+    }
+}
---
+++ DataStructures/MyStack.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class MyStack
+    {
+        private object?[] arrStack;
+        private int _size;
+
+        public int Size 
+        {
+            get { return _size; }
+        }
+
+
+        private const int _defaultCapacity = 10;
+
+        MyStack()
+        {
+            arrStack = new object[_defaultCapacity];
+            _size = -1;
+        }
+
+     
+        MyStack(int capacity)
+        {
+            arrStack = new object[capacity];
+            _size = -1;
+        }
+        private bool IsFull()
+        {
+            return _size == arrStack.Length - 1;
+        }
+        private bool IsEmpty()
+        {
+            return _size == -1;
+        }
+
+        public object Push(object value)
+        {
+            if(IsFull())
+            {
+               throw new Exception($"Stack limit {Size} - size exception");
+            }
+            ++_size;
+            arrStack[_size] = value;
+            return arrStack[_size];
+        }
+
+        public object Pop(object value)
+        {
+            int a = 0;
+            if (IsEmpty())
+            {
+                throw new Exception($"An Object not created , null exception");
+            }
+
+            object poppedValue = arrStack[_size];
+            arrStack[_size] = null;
+            _size--;
+
+            return poppedValue;
+        }
+
+    }
+}
